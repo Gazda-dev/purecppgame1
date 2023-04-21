28a731cd02bf85aaa7a585fa5a69bb2a3818eec8
Gazda-dev <karimzheadshot@gmail.com>
2023-04-21 20:20:51 +0200

    main

--- raylib-vscode-template-main/main.cpp
+++ raylib-vscode-template-main/main.cpp
@@ -2,34 +2,71 @@
 
 int main()
 {
-    // Inicjalizacja okna
-    InitWindow(350, 200, "Yo");
+    //window
+    int width{800};
+    int height{450};
 
-    // Pobranie czasu rozpoczęcia programu
-    double startTime = GetTime();
+    //circle
+    int circle_x{width/2};
+    int circle_y{height/2};
+    int circle_radius{20};
 
-    // Pętla główna
-    while (!WindowShouldClose())
+    int l_circle_x{circle_x-circle_radius};
+    int r_circle_x{circle_x+circle_radius};
+    int u_circle_y{circle_y+circle_radius};
+    int b_circle_y{circle_y-circle_radius};
+    
+    //axe
+    int axe_x{400};
+    int axe_y{0};
+    int direction{10};
+    int axe_length{50};
+
+    int l_axe_x{axe_x};
+    int r_axe_x{axe_length + axe_x};
+    int u_axe_y{axe_y};
+    int b_axe_y{axe_y+axe_length};
+
+    bool collision_with_axe = false;
+
+    InitWindow(width, height, "Yo");
+    SetTargetFPS(60);
+
+    while(WindowShouldClose() != true)
     {
-        // Aktualizacja logiki gry tutaj
-
-        // Rozpoczęcie rysowania
         BeginDrawing();
+        ClearBackground(WHITE);
 
-        // Wyczyszczenie ekranu
-        ClearBackground(RAYWHITE);
-
-        // Rysowanie tutaj
-
-        // Zakończenie rysowania i wyświetlenie wyniku
-        EndDrawing();
-
-        // Sprawdzenie, czy minęło 5 sekund
-        if (GetTime() - startTime >= 5.0)
+        if(collision_with_axe)
         {
-            // Zamknięcie okna
-            CloseWindow();
+            DrawText("Game Over!", circle_x, circle_y, 20, RED);
         }
+        else
+        {
+            //game start
+            DrawCircle(circle_x, circle_y, circle_radius, BLUE);
+            DrawRectangle(axe_x, axe_y, 50, axe_length, RED);
+
+            axe_y += direction;
+            if(axe_y > height || axe_y < 0)
+            {
+            direction = - direction;
+            }
+
+            if(IsKeyDown(KEY_D) && circle_x < width)
+            {
+            circle_x += 10;
+            }
+            if(IsKeyDown(KEY_A) && circle_x > 0)
+            {
+            circle_x -= 10;
+            }
+
+            //game end
+        }
+        
+
+        EndDrawing();
     }
 
     return 0;
